2c0192bef3d1f3dd619cc06bf6d105a13afe5bdb
zhaozhiguang <zhaozhiguang@users.noreply.github.com>
2017-12-21 16:28:05 +0800

    线程池

--- spider/src/main/java/com/zhaozhiguang/component/spider/SimpleImageParse.java
+++ spider/src/main/java/com/zhaozhiguang/component/spider/SimpleImageParse.java
@@ -1,9 +1,19 @@
 package com.zhaozhiguang.component.spider;
 
+import com.zhaozhiguang.component.threads.CustomThreadPoolFactory;
+
+import java.util.concurrent.ExecutorService;
+
 /**
  * 简单解析图片下载实现
  */
 public class SimpleImageParse implements ImageParseable {
+
+    private ExecutorService executorService;
+
+    public SimpleImageParse(){
+        executorService = CustomThreadPoolFactory.build(CustomThreadPoolFactory.ThreadPoolType.FIXED_THREADPOOL);
+    }
 
     @Override
     public void parseImage(String url) {
